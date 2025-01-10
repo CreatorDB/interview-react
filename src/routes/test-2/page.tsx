2be830c6a36db4aa74207340fec641ce8d51e51e
stevencjyu <stevenyu@creatordb.app>
2025-01-10 17:34:33 +0800

    feat: update test2

--- src/routes/test-2/page.tsx
+++ src/routes/test-2/page.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import shuffle from 'lodash/shuffle';
 
-let renderCount = 0;
+import { QuestionBox } from '../../components/question-box';
+
+const showQuestion2 = true;
 
 export const Test2Page = () => {
-  // increment render count on mount
-  console.log(`Render count: ${++renderCount}`);
+  const renderCountRef = React.useRef<number>(0);
+  console.log(`Render count: ${++renderCountRef.current}`);
 
-  // name state
   const [name, setName] = React.useState<string>('');
 
-  // handle name change
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('change');
     setName(e.target.value);
   };
 
-  // handle submit
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     window.alert(`Hello, ${name}!`);
@@ -23,28 +24,54 @@ export const Test2Page = () => {
   // reset render count on unmount
   React.useEffect(() => {
     return () => {
-      renderCount = 0;
+      renderCountRef.current = 0;
     };
   }, []);
 
   return (
     <>
-      <h1 className="text-lg font-bold">Show me how to not make this component re-render</h1>
-      <form className="flex flex-col gap-y-4" onSubmit={handleSubmit}>
-        {/* input */}
-        <div>
-          <input
-            type="text"
-            value={name}
-            className="border border-gray-300 px-2 outline-none"
-            onChange={handleChange}
-          />
-        </div>
-        {/* submit button */}
-        <button type="submit" className="rounded-full border-blue-700 bg-blue-400 px-2 text-white">
-          Submit
-        </button>
-      </form>
+      <div className="mx-auto w-[480px]">
+        <QuestionBox index={1} text="Please explain what happened when user typing in the input." />
+        <form className="flex flex-col gap-y-4" onSubmit={handleSubmit}>
+          {/* input */}
+          <div>
+            <input
+              type="text"
+              value={name}
+              className="w-full border border-gray-300 px-2 outline-none"
+              onChange={handleChange}
+            />
+          </div>
+          {/* submit button */}
+          <button type="submit" className="rounded-full border-blue-700 bg-blue-400 px-2 text-white">
+            Submit
+          </button>
+        </form>
+
+        {showQuestion2 && (
+          <div className='mt-8'>
+            <QuestionBox
+              index={2}
+              text="If there's lot's of element on this page, how would you improve the performance for user typing."
+            />
+            <HeavyElement count={12000} />
+          </div>
+        )}
+      </div>
     </>
   );
 };
+
+function HeavyElement({ count }: { count: number }) {
+  return (
+    <div className="my-2 flex flex-wrap gap-2">
+      {shuffle(
+        Array.from({ length: count }, (_, i) => (
+          <span key={i} className="border border-gray-300 p-2">
+            <span>{i}</span>
+          </span>
+        )),
+      )}
+    </div>
+  );
+}
